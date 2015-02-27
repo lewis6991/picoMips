7518d56fa16e64ffa6e8a5cc05e12c9b1bd01fc7
lewis6991 <lewis6991@gmail.com>
2015-02-27 21:49:08 +0000

    Implement program_counter using multipliers.

--- program_counter.sv
+++ program_counter.sv
@@ -10,10 +10,30 @@ module program_counter(
     output logic [6:0] ProgramCounter
 );
 
+`ifdef SIM
 always_ff @ (posedge Clock, negedge nReset)
     if (~nReset)
         ProgramCounter <= #20 0;
     else if (~PCHold)
         ProgramCounter <= #20 ProgramCounter + 7'd1;
+`else
+lpm_mult lpm_mult_component (
+    .clken (1'b0                                 ),
+    .clock (Clock                                ),
+    .dataa ({1'd0, ProgramCounter, 7'd0, ~PCHold}),
+    .datab ({8'd1, 8'd1}                         ),
+    .result({1'd0, ProgramCounter, tmp}          ),
+    .aclr  (~nReset                              ),
+    .sum   (1'b0                                 )
+);
+defparam
+lpm_mult_component.lpm_hint = "INPUT_B_IS_CONSTANT=NO,DEDICATED_MULTIPLIER_CIRCUITRY=YES,MAXIMIZE_AREA=1",
+lpm_mult_component.lpm_pipeline = 1,
+lpm_mult_component.lpm_representation = "SIGNED",
+lpm_mult_component.lpm_type = "LPM_MULT",
+lpm_mult_component.lpm_widtha = 16,
+lpm_mult_component.lpm_widthb = 16,
+lpm_mult_component.lpm_widthp = 16;
+`endif
 
 endmodule
