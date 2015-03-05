513ee5bec852e53529d29d74f01fae0a12381ffc
Lewis Russell <lewis6991@gmail.com>
2015-03-05 15:10:37 +0000

    Added wrapper files for cyclone III implementation.

---
+++ counter.sv
@@ -0,0 +1,17 @@
+// counter for slow clock
+module counter #(
+    parameter n = 12
+) //clock divides by 2^n, adjust n if necessary
+(
+    input logic fastclk,
+    output logic clk
+);
+
+logic [n-1:0] count;
+
+always_ff @(posedge fastclk)
+    count <= count + 1;
+
+assign clk = count[n-1]; // slow clock
+
+endmodule
